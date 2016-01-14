562d8e47c44b0e4a941ccdaaaeb58cfe054d816a
Sam Chandler <samuelvanchandler@gmail.com>
2016-01-14 15:51:46 -0700

    added index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Data Design Project</title>
+	</head>
+	<body>
+		<h1>Staples</h1>
+		<h3>Sharpies.....</h3>
+	</body>
+</html>
