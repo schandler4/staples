6df260a3d9319af711b066fa7d1ea0363254e279
Sam Chandler <samuelvanchandler@gmail.com>
2016-01-20 08:08:54 -0700

    added index.php

--- index.php
+++ index.php
@@ -5,7 +5,27 @@
 		<title>Data Design Project</title>
 	</head>
 	<body>
-		<h1>Staples</h1>
+		<h1>Staples ordering subsystems</h1>
 		<h3>Sharpies.....</h3>
+		<p>
+			The Staples ordering subsystem allows customers to order
+			office supplies over the web.  They do not provide
+			delievery services, rather the customer is directed to pick
+			up their order from a store location. It seems to be geared
+			towards larger orders of bulk office supplies.  The persona
+			this service is intended for would most likely be a small
+			business owner, not large enough to justify one of the office
+			supply delivery services, but large enough that an online
+			order for store pick up would save them a considerable amount
+			of time. By not having to hunt around the store, wait in lines,
+			or perform any financial transactions, they can get back to the
+			important work of writing on things with sharpies.
+		</p>
+		<p>
+			This service wouldn't make much sense for small orders, since
+			the time saved and conviencey would be neglagable.  Also, large
+			corporate businesses would benifit more from a wholesale supplier
+			that offers delievery services.
+		</p>
 	</body>
 </html>
