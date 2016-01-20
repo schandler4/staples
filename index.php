0856d6ff103f288a41864d311de7601f2ec4ae88
Sam Chandler <samuelvanchandler@gmail.com>
2016-01-20 08:29:22 -0700

    added index.php

--- index.php
+++ index.php
@@ -5,27 +5,38 @@
 		<title>Data Design Project</title>
 	</head>
 	<body>
-		<h1>Staples ordering subsystems</h1>
-		<h3>Sharpies.....</h3>
-		<p>
-			The Staples ordering subsystem allows customers to order
+		<h1>Data design of the Staples ordering subsystems</h1>
+		<h2>Sharpies.....</h2>
+		<h3>Persona</h3>
+		<p>The Staples ordering subsystem allows customers to order
 			office supplies over the web.  They do not provide
 			delievery services, rather the customer is directed to pick
 			up their order from a store location. It seems to be geared
 			towards larger orders of bulk office supplies.  The persona
 			this service is intended for would most likely be a small
-			business owner, not large enough to justify one of the office
-			supply delivery services, but large enough that an online
-			order for store pick up would save them a considerable amount
-			of time. By not having to hunt around the store, wait in lines,
-			or perform any financial transactions, they can get back to the
+			business owner or office manager, let's call him Bob, of a
+			business not large enough to justify one of the offic supply
+			delivery services,but large enough that an online order for
+			store pick up would save them a considerable amount of time.
+			By not having to hunt around the store, wait in lines,or
+			perform any financial transactions, they can get back to the
 			important work of writing on things with sharpies.
 		</p>
-		<p>
-			This service wouldn't make much sense for small orders, since
-			the time saved and conviencey would be neglagable.  Also, large
-			corporate businesses would benifit more from a wholesale supplier
-			that offers delievery services.
+		<h3>Use Case</h3>
+		<p>Bob is the owner of a small business, it's a paper pushing kind
+			of business, like medical billing, or tax returns. He has 10 other
+			employees other than himself. He show's upon Monday morning and
+			realizes that the office is low on lot's of supplies; envolpes,
+			pens, printer paper, ink toner, staples (of course), and sharpies.
+			He get's online from his office desktop and goes to Staples.com.
+			from here he enter's the name of the supplies he wants in the search
+			bar, and quickly addes the above supplies in the quantities he needs.
+			He does this often, so he has a profile set up, and completes the
+			financial transaction by using his saved credit card.  He then tells
+			his secretary to pick them up on her lunch break.
+
+
+
 		</p>
 	</body>
 </html>
